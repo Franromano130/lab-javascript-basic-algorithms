33f9bfaa712b2955d105272c2cda899e4f8d42f5
Francisco Romano <franromano130@gmail.com>
2023-04-11 22:08:18 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,7 +1,31 @@
-// Iteration 1: Names and Input
+console.log("I'm ready!")
 
+let hacker1 = "Jason";
+let hacker2 = "Mario";
+let driver =
+"the driver name is" + " " + hacker1;
+console.log(driver)
+let navigator = 
+"the navigators name is" + " " + hacker2; {
+console.log(navigator)
+}
+const str = "Jason"
+const str2 = "Mario"
+if (str.length = str2.length){
+    console.log("Wow, you both have equally long names, XX characters!")
+}
+else if (str.length > str2.length){
+    console.log("The driver has the longest name, it has XX characters.")
+}
+else if (str.length < str2.length){
+    console.log("It seems that the navigator has the longest name, it has XX characters.")
+}
+const sentence1= "j a s o n"
+console.log(sentence1.toUpperCase())
 
-// Iteration 2: Conditionals
-
-
-// Iteration 3: Loops
+let reversed = "";
+ 
+for (let i = str2.length - 1; i >= 0; i--) {
+  reversed += str2[i];
+}
+console.log(reversed);
